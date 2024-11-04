311b8d3b4ea708b52b5b109cb406deb54ad28b77
mariamtamerr <memariam5@outlook.com>
2024-11-04 17:44:44 +0200

    added composables and modules

--- components/Info.vue
+++ components/Info.vue
@@ -1,34 +1,61 @@
 <template>
-    <div>
-        <div class="max-w-sm space-y-3 mx-auto mt-5">
-  <div class="relative">
-    <input type="email" class="peer py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter name">
-    <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-      <svg class="shrink-0 size-4 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-      </svg>
-    </div>
-  </div>
+  <div>
+    <div class="max-w-sm space-y-3 mx-auto mt-5">
+      <div class="relative">
+        <input
+          type="email"
+          class="peer py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none d:bg-neutral-700 d:border-transparent d:text-neutral-400 d:placeholder-neutral-500 d:focus:ring-neutral-600"
+          placeholder="Enter name"
+        />
+        <div
+          class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none"
+        >
+          <svg
+            class="shrink-0 size-4 text-gray-500 d:text-neutral-500"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+      </div>
 
-  <div class="relative">
-    <input type="phone" class="peer py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter mobile">
-    <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-        <svg class="shrink-0 size-4 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <div class="relative">
+        <input
+          type="phone"
+          class="peer py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none d:bg-neutral-700 d:border-transparent d:text-neutral-400 d:placeholder-neutral-500 d:focus:ring-neutral-600"
+          placeholder="Enter mobile"
+        />
+        <div
+          class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none"
+        >
+          <svg
+            class="shrink-0 size-4 text-gray-500 d:text-neutral-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
             <rect x="7" y="2" width="10" height="20" rx="2" ry="2"></rect>
             <path d="M11 18h2"></path>
-        </svg>
-
+          </svg>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-    </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- components/Modal.vue
+++ components/Modal.vue
@@ -1,103 +1,110 @@
 <template>
-    <div>
-      <!-- Button to open the modal -->
-      <button
-        type="button"
-        class="mt-10 bg-[#D71F29] text-white px-4 py-2 rounded hover:bg-[#850e14] transition duration-200"
-        @click="openModal"
-      >
-        Make A Reservation
-      </button>
-  
-      <!-- Modal -->
-      <div
-        v-if="isModalOpen"
-        class="hs-overlay fixed top-0 left-0 w-full h-full z-[80] opacity-100 overflow-x-hidden transition-all overflow-y-auto pointer-events-auto"
-        role="dialog"
-        tabindex="-1"
-        aria-labelledby="hs-basic-modal-label"
-      >
-        <div class="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
-          <div class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
-            <div class="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
-              <h3 id="hs-basic-modal-label" class="font-bold  mx-auto text-gray-800 dark:text-white">
-                Reserve A Date 
-              </h3>
-              <button
-                type="button"
-                class="inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
-                aria-label="Close"
-                @click="closeModal"
-              >
-                <span class="sr-only">Close</span>
-                <svg
-                  class="shrink-0 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M18 6L6 18"></path>
-                  <path d="M6 6l12 12"></path>
-                </svg>
-              </button>
-            </div>
-            <div class="p-4  overflow-y-auto text-center">
-                <!-- <div class="left"> -->
-                    <p class="mt-1 text-gray-800 dark:text-neutral-400">
-                        Once you choose a date and confirm your reservation, customer service will contact you shortedly.
-                    </p>
-                <!-- </div> -->
-              <div class="right mt-5 ">
-                <Calender/>
-              </div>
+  <div>
+    <!-- Button to open the modal -->
+    <button
+      type="button"
+      class="mt-10 bg-[#D71F29] text-white px-4 py-2 rounded hover:bg-[#850e14] transition duration-200"
+      @click="openModal"
+    >
+      Make A Reservation
+    </button>
 
-              <div class="info">
-                <Info/>
+    <!-- Modal -->
+    <div
+      v-if="isModalOpen"
+      class="hs-overlay fixed top-0 left-0 w-full h-full z-[80] opacity-100 overflow-x-hidden transition-all overflow-y-auto pointer-events-auto"
+      role="dialog"
+      tabindex="-1"
+      aria-labelledby="hs-basic-modal-label"
+    >
+      <div class="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+        <div
+          class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto d:bg-neutral-800 d:border-neutral-700 d:shadow-neutral-700/70"
+        >
+          <div
+            class="flex justify-between items-center py-3 px-4 border-b d:border-neutral-700"
+          >
+            <h3
+              id="hs-basic-modal-label"
+              class="font-bold mx-auto text-gray-800 d:text-white"
+            >
+              Reserve A Date
+            </h3>
+            <button
+              type="button"
+              class="inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
+              aria-label="Close"
+              @click="closeModal"
+            >
+              <span class="sr-only">Close</span>
+              <svg
+                class="shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6L6 18"></path>
+                <path d="M6 6l12 12"></path>
+              </svg>
+            </button>
+          </div>
+          <div class="p-4 overflow-y-auto text-center">
+            <!-- <div class="left"> -->
+            <p class="mt-1 text-gray-600 d:text-neutral-400">
+              Once you choose a date and confirm your reservation, customer
+              service will contact you shortedly.
+            </p>
+            <!-- </div> -->
+            <div class="right mt-5">
+              <Calender />
+            </div>
 
-              </div>
+            <div class="info">
+              <Info />
             </div>
-            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
-              <button
-                type="button"
-                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-300 focus:outline-none focus:bg-gray-50"
-                @click="closeModal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-slate-800 text-white hover:bg-slate-500 "
-              >
-                Save changes
-              </button>
-            </div>
+          </div>
+          <div
+            class="flex justify-end items-center gap-x-2 py-3 px-4 border-t d:border-neutral-700"
+          >
+            <button
+              type="button"
+              class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-300 focus:outline-none focus:bg-gray-50"
+              @click="closeModal"
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-slate-800 text-white hover:bg-slate-500"
+            >
+              Save changes
+            </button>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  // State variable to track if the modal is open
-  const isModalOpen = ref(false);
-  
-  // Function to open the modal
-  const openModal = () => {
-    isModalOpen.value = true;
-  };
-  
-  // Function to close the modal
-  const closeModal = () => {
-    isModalOpen.value = false;
-  };
-  </script>
-  
-  
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// State variable to track if the modal is open
+const isModalOpen = ref(false);
+
+// Function to open the modal
+const openModal = () => {
+  isModalOpen.value = true;
+};
+
+// Function to close the modal
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+</script>
